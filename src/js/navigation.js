69afab2ce40c6ed6174a52723827c6b7639759cb
Ryan Allen <ryansallen53@gmail.com>
2024-10-21 23:17:06 +0100

    Create and update nav functions

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -13,3 +13,42 @@ export function disableNav(dots, boolean) {
         dot.disabled = boolean;
     });
 }
+
+export function createDot(slide, index, slideWidth) {
+    const dot = document.createElement("button");
+    dot.classList.add("bd-carousel__nav-dot");
+
+    const timer = document.createElement("div");
+    timer.classList.add("bd-carousel__nav-dot-timer");
+
+    const text = document.createElement("p");
+    const slideTitle = slide.querySelector(".wp-block-heading")
+    if (slideTitle) {
+        text.innerText = slideTitle.innerText;;
+    }
+
+    // If it's the first slide, set it as active and position it at 0px
+    if (index === 0) {
+        slide.classList.add("active");
+        dot.classList.add("active");
+        slide.style.transform = `translateX(${0}px)`;
+    } else {
+        slide.style.transform = `translateX(${slideWidth}px)`;
+    }
+
+    dot.appendChild(timer);
+    dot.appendChild(text);
+
+    return dot;
+}
+
+export function updateNav(currentDot, targetDot, autoplayTimer) {
+    currentDot.classList.remove("active");
+    targetDot.classList.add("active");
+
+    const oldTimer = currentDot.querySelector(".bd-carousel__nav-dot-timer");
+    oldTimer.style.animation = ``;
+
+    const newTimer = targetDot.querySelector(".bd-carousel__nav-dot-timer");
+    newTimer.style.animation = `timerAnimation ${autoplayTimer}ms linear forwards`;
+};
