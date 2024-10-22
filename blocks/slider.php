3e5a499dbe1e9f91d54aff7a7f20d3608d264274
Ryan Allen <ryansallen53@gmail.com>
2024-10-22 12:05:51 +0100

    Apply speed attributes

--- blocks/slider.php
+++ blocks/slider.php
@@ -1,4 +1,5 @@
-<div class="bd-carousel">
+<div class="bd-carousel" data-speed="<?php echo $attributes['transitionSpeed'] ?>"
+    data-autoplay-timer="<?php echo $attributes['autoplaySpeed'] ?>">
     <div class="bd-carousel__wrapper">
         <?php echo $contents; ?>
     </div>
